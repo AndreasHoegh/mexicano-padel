987373dd84e873701631dd87a7f9f88c14c0281a
Andreas <201908525@post.au.dk>
2024-12-06 22:11:06 +0100

    bug

--- components/App.tsx
+++ components/App.tsx
@@ -21,7 +21,7 @@ interface Match {
 
 export default function App() {
   const [numberOfPlayers, setNumberOfPlayers] = useState<number>(0);
-  // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [names, setNames] = useState<string[]>([]);
   const [matches, setMatches] = useState<Match[]>([]);
   const [scores, setScores] = useState<{ [key: string]: number }>({});
