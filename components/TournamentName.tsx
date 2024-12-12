5e9d437a1fc5b8250e157dbb487498b4a1663668
Andreas <201908525@post.au.dk>
2024-12-12 20:44:34 +0100

    bug

--- components/TournamentName.tsx
+++ components/TournamentName.tsx
@@ -1,24 +1,5 @@
-import React from "react";
-
 function TournamentName() {
-  return (
-    <form
-      className="flex flex-col sm:flex-row mx-8 justify-center gap-2 mt-8"
-      onSubmit={handleSubmit(handleTournamentNameSubmit)}
-    >
-      <input
-        className="border-2 border-slate-500 p-1"
-        type="text"
-        placeholder="Enter Tournament Name"
-        defaultValue="Mock Tournament" // Default værdi
-        {...register("tournamentName", { required: true })}
-        autoFocus
-        onFocus={(e) => e.target.select()} // Marker hele teksten når feltet får fokus
-      />
-
-      <Button type="submit">Set Tournament Name</Button>
-    </form>
-  );
+  return;
 }
 
 export default TournamentName;
